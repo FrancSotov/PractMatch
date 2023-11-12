f3a38e7e2765fa8fe3f531ef7ada1fdff0074f1c
M4r10 <eltunox123@gmail.com>
2023-11-12 00:18:54 -0300

    conexion a firebase con auth y firestore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production:false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBU4py7x3taBZRvaZQ6r08ANjER4AgoYxo",
+    authDomain: "practmatch.firebaseapp.com",
+    projectId: "practmatch",
+    storageBucket: "practmatch.appspot.com",
+    messagingSenderId: "584537492478",
+    appId: "1:584537492478:web:b13b1d3187da8fdc880eaf",
+    measurementId: "G-LD15VEVNGC"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production:false,
+   firebaseConfig : {
+    apiKey: "AIzaSyBU4py7x3taBZRvaZQ6r08ANjER4AgoYxo",
+    authDomain: "practmatch.firebaseapp.com",
+    projectId: "practmatch",
+    storageBucket: "practmatch.appspot.com",
+    messagingSenderId: "584537492478",
+    appId: "1:584537492478:web:b13b1d3187da8fdc880eaf",
+    measurementId: "G-LD15VEVNGC"
+  }
 };
 
 /*
