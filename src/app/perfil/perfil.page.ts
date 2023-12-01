604a9ebbb2d34605872d5eba0ff7001b5fdc8f7b
M4r10 <eltunox123@gmail.com>
2023-12-01 20:22:59 -0300

    k saen de lindura

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-perfil',
-  templateUrl: './perfil.page.html',
-  styleUrls: ['./perfil.page.scss'],
+  templateUrl: 'perfil.page.html',
+  styleUrls: ['perfil.page.scss'],
 })
-export class PerfilPage implements OnInit {
+export class PerfilPage {
 
-  constructor() { }
+  nombre: string = 'Juan Perez';
+  carrera: string = 'Ing. Informática';
+  nuevoNombre: string = '';
+  nuevaCarrera: string = '';
+  modificando: boolean = false;
+  palabraClave: string = '';
+  palabrasClavesDisponibles: string[] = ['Trabajo en Equipo', 'Python', 'Java', 'Frontend', 'Backend'];
+  palabrasClavesSeleccionadas: string[] = [];
 
-  ngOnInit() {
+  habilitarModificacion() {
+    this.modificando = true;
   }
 
+  guardarCambios() {
+    this.nombre = this.nuevoNombre || this.nombre;
+    this.carrera = this.nuevaCarrera || this.carrera;
+    this.modificando = false;
+  }
+
+  agregarPalabraClave() {
+    if (this.palabraClave && !this.palabrasClavesSeleccionadas.includes(this.palabraClave)) {
+      this.palabrasClavesSeleccionadas.push(this.palabraClave);
+    }
+  }
 }
