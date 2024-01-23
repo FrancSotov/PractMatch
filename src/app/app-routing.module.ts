896b1f13d5155d0b064f75a01f0b7d9597128e9f
Panchi01~ <franc.sotov@gmail.com>
2024-01-23 11:50:58 -0300

    avances con front, espero confirmacion para preparar front final.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,6 +21,7 @@ const routes: Routes = [
     path: 'sign-up',
     loadChildren: () => import('./sign-up/sign-up.module').then(m => m.SignUpPageModule)
   },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
