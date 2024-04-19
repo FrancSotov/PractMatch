47b191593208d7676d3b85d23124480f65a6e629
M4r10 <eltunox123@gmail.com>
2024-04-19 11:23:53 -0400

    aa

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'Tinderlaboral',
-  appName: 'tinderLaboral',
+  appId: 'pract.match',
+  appName: 'PractMatch',
   webDir: 'www',
   server: {
     androidScheme: 'https'
