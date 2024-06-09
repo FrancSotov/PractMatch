7a02f6334faff939993b9d29a49f621421ef0fb5
M4r10 <eltunox123@gmail.com>
2024-06-09 16:28:59 -0400

    Revert "aa"
    This reverts commit 47b191593208d7676d3b85d23124480f65a6e629.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'pract.match',
-  appName: 'PractMatch',
+  appId: 'Tinderlaboral',
+  appName: 'tinderLaboral',
   webDir: 'www',
   server: {
     androidScheme: 'https'
